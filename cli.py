e00e1bbb66b63d3476769e8dcc28467b723eab44
slippedandmissed <gbt505@hotmail.com>
2021-05-08 22:21:36 +0100

    Added command line interpreter for turing machine programs

---
+++ cli.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3.9
+
+import sys
+
+tape = [""]
+if len(sys.argv) > 2:
+    tape = list(sys.argv[2])
+    for i in range(len(tape)):
+        if tape[i] == " ":
+            tape[i] = ""
+
+program = sys.argv[1]
+
+
+state = "start"
+currentIndex = 0
+
+rules = {}
+
+with open(program, encoding="utf-8") as transitionTable:
+    data = [x.strip() for x in transitionTable.read().strip().split("\n") if not x.startswith("#")]
+for index, i in enumerate(data):
+    try:
+        parts = i.split(" = ")
+        oldState, read = parts[0][2:-1].split(",")
+        write, arrow, newState = parts[1][1:-1].split(",")
+        direction = "<" if arrow == "←" else ">"
+        if oldState not in rules:
+            rules[oldState] = {}
+        rules[oldState][read] = [write, direction, newState]
+    except:
+        print("Mangled rule: '%s' on line %i" % (i, index+1))
+        sys.exit()
+
+currentRule = "Program terminated"
+if state in rules:
+    if tape[currentIndex] in rules[state]:
+        write, direction, newState = rules[state][tape[currentIndex]]
+        arrow = "←" if direction == "<" else "→"
+        currentRule = "Δ(%s,%s) = (%s,%s,%s)" % (state, tape[currentIndex], write, arrow, newState)
+
+
+def advance():
+    global state
+    global tape
+    global currentRule
+    global currentIndex
+    if state in rules:
+        if tape[currentIndex] in rules[state]:
+            write, direction, newState = rules[state][tape[currentIndex]]
+            transitioning = -1 if direction == "<" else 1
+            arrow = "←" if direction == "<" else "→"
+            state = newState
+            tape[currentIndex] = write
+            currentIndex += transitioning
+            if currentIndex < 0:
+                currentIndex += 1
+                tape = [""] + tape
+            if currentIndex >= len(tape):
+                tape.append("")
+            return True
+    return False
+
+stateWidth = 20
+
+span = 20
+
+change = True
+while change:
+    sname = state.ljust(stateWidth)
+    if len(sname) > stateWidth:
+        sname = sname[:stateWidth-3]+"..."
+    print(f"{sname}: ", end="")
+    line = "..."
+    underline = " "*(stateWidth+5)
+    for i in range(max(0, currentIndex-span), min(currentIndex+span+1, len(tape))):
+        x = tape[i]
+        line += " " if x == "" else x
+        underline += (chr(8254) if i == currentIndex else " ")*(1 if x == "" else len(x))
+        nonBlank = True
+    print(line+"...")
+    print(underline)
+    change = advance()
